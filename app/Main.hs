720e5ddc33832e27d4a7fe75d4c7667f45c32c51
lxr205867 <25113040017@m.fudan.edu.cn>
2026-05-31 19:27:54 +0800

    feat: implement backend TODO evaluation

--- app/Main.hs
+++ app/Main.hs
@@ -5,39 +5,106 @@ import MiniLang.Repl (repl)
 import MiniLang.Parsing.Lexer (lexProgram)
 import MiniLang.Parsing.Parser (parseProgram)
 import System.Environment (getArgs)
+import System.Exit (exitFailure)
+
+data Mode
+  = Run
+  | Debug
+  | Tokens
+  | Ast
+  | Repl
 
 main :: IO ()
 main = do
   args <- getArgs
-  case args of
-    [] -> runFile "examples/showcase.minilang"
-    ["repl"] -> repl
-    ["--repl"] -> repl
-    [sampleFile] -> runFile sampleFile
-    _ -> putStrLn "Usage: MiniLang [repl|--repl|FILE]"
+  case parseArgs args of
+    Left message -> failWith message
+    Right (Repl, _) -> repl
+    Right (mode, sourceFile) -> do
+      source <- readFile sourceFile
+      runMode mode sourceFile source
 
-runFile :: FilePath -> IO ()
-runFile sampleFile = do
-  source <- readFile sampleFile
-  putStrLn ("Source file: " ++ sampleFile)
+parseArgs :: [String] -> Either String (Mode, FilePath)
+parseArgs args =
+  case args of
+    [] -> Right (Debug, "examples/showcase.minilang")
+    ["repl"] -> Right (Repl, "")
+    ["--repl"] -> Right (Repl, "")
+    [sourceFile] -> Right (Run, sourceFile)
+    ["--debug", sourceFile] -> Right (Debug, sourceFile)
+    ["--tokens", sourceFile] -> Right (Tokens, sourceFile)
+    ["--ast", sourceFile] -> Right (Ast, sourceFile)
+    _ -> Left usage
+
+usage :: String
+usage =
+  unlines
+    [ "Usage:"
+    , "  MiniLang"
+    , "  MiniLang FILE"
+    , "  MiniLang --debug FILE"
+    , "  MiniLang --tokens FILE"
+    , "  MiniLang --ast FILE"
+    , "  MiniLang repl"
+    , "  MiniLang --repl"
+    ]
+
+runMode :: Mode -> FilePath -> String -> IO ()
+runMode mode sourceFile source =
+  case mode of
+    Run -> runProgramOnly source
+    Debug -> runDebug sourceFile source
+    Tokens -> printTokens source
+    Ast -> printAst source
+    Repl -> repl
+
+runProgramOnly :: String -> IO ()
+runProgramOnly source =
+  case parseProgram source of
+    Left err -> failWith (show err)
+    Right program ->
+      case runProgram program of
+        Left err -> failWith (show err)
+        Right (output, _env) -> mapM_ putStrLn output
+
+printTokens :: String -> IO ()
+printTokens source =
+  case lexProgram source of
+    Left err -> failWith (show err)
+    Right tokens -> mapM_ print tokens
+
+printAst :: String -> IO ()
+printAst source =
+  case parseProgram source of
+    Left err -> failWith (show err)
+    Right program -> print program
+
+runDebug :: FilePath -> String -> IO ()
+runDebug sourceFile source = do
+  putStrLn ("Source file: " ++ sourceFile)
   putStrLn ""
   putStrLn "=== Source ==="
   putStrLn source
   putStrLn "=== Lexer Output ==="
 
   case lexProgram source of
-    Left err -> print err
+    Left err -> failWith (show err)
     Right tokens -> do
       mapM_ print tokens
       putStrLn "=== Parser Output ==="
       case parseProgram source of
-        Left err -> print err
+        Left err -> failWith (show err)
         Right program -> do
           print program
           putStrLn "=== Eval Output ==="
           case runProgram program of
-            Left err -> print err
+            Left err -> failWith (show err)
             Right (output, env) -> do
               mapM_ putStrLn output
               putStrLn "=== Final Env ==="
               print env
+
+failWith :: String -> IO ()
+failWith message = do
+  putStrLn message
+  exitFailure
--- app/MiniLang/Backend/Value.hs
+++ app/MiniLang/Backend/Value.hs
@@ -1,5 +1,6 @@
 module MiniLang.Backend.Value where
 
+import Data.List (intercalate)
 import qualified Data.Map as Map
 import MiniLang.Parsing.Syntax (Stmt)
 
@@ -7,23 +8,35 @@ type Env = Map.Map String Value
 
 data Value
   = VInt Int
+  | VFloat Double
   | VBool Bool
   | VString String
   | VUnit
   | VFunction [String] [Stmt] Env
+  | VConstructor String [Value]
+  | VConstructorFunction String Int
 
 instance Eq Value where
   VInt left == VInt right = left == right
+  VFloat left == VFloat right = left == right
   VBool left == VBool right = left == right
   VString left == VString right = left == right
   VUnit == VUnit = True
   VFunction {} == VFunction {} = False
+  VConstructor leftName leftValues == VConstructor rightName rightValues =
+    leftName == rightName && leftValues == rightValues
+  VConstructorFunction leftName leftArity == VConstructorFunction rightName rightArity =
+    leftName == rightName && leftArity == rightArity
   _ == _ = False
 
 instance Show Value where
   show (VInt value) = show value
+  show (VFloat value) = show value
   show (VBool True) = "true"
   show (VBool False) = "false"
   show (VString value) = show value
   show VUnit = "unit"
   show VFunction {} = "<function>"
+  show (VConstructor name []) = name
+  show (VConstructor name values) = name ++ "(" ++ intercalate ", " (map show values) ++ ")"
+  show (VConstructorFunction name _) = "<constructor " ++ name ++ ">"
